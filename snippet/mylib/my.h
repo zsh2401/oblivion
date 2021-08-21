0f6afc2e7cb8f8069a3f174a6e15ff108aab8b2a
zsh2401 <zsh2401@163.com>
2021-08-22 07:37:26 +0800

    call different api

--- snippet/mylib/my.h
+++ snippet/mylib/my.h
@@ -1,6 +1,6 @@
 #ifndef __MY_LIB_H
 #define __MY_LIB_H
 
-int my_add(int a,int b);
-
+int my_add(int a, int b);
+void alert(char *msg);
 #endif // !__MY_LIB_H
--- snippet/mylib/src/add.c
+++ snippet/mylib/src/add.c
@@ -1,5 +1,21 @@
 #include "../my.h"
+
+#ifdef WIN32
+#include <windows.h>
+#include <winuser.h>
+#else
+#include <stdio.h>
+#endif
+
 int my_add(int a, int b)
 {
     return a + b;
+}
+void alert(char *msg)
+{
+#ifdef WIN32
+    MessageBox(NULL, TEXT(msg), TEXT("HELLO"), MB_OK);
+#else
+    printf("%s\n", msg);
+#endif
 }
